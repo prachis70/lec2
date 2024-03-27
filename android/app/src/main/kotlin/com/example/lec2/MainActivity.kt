909671c56532133fce2126b83f619330a86cad9f
Tc Family <singhprachi9496@gmail.com>
2024-03-27 20:08:47 +0530

    27/3/2024

---
+++ android/app/src/main/kotlin/com/example/lec2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lec2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          centerTitle: true,
+          leading: Icon(Icons.menu,color: Colors.white,),
+          title: Text('Flutter App',style: TextStyle(color: Colors.white),),
+
+
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                '     Red & White Group of institude:\n'
+                    'One Step in Changing Education Chain...',
+                style: TextStyle(fontSize: 20,color: Colors.red),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
